cb7c22a974d1966dc29b0549b1f450e4575806bb
1139221421 <1139221421@qq.com>
2018-03-28 17:00:36 +0800

    spring异步线程

---
+++ src/main/java/com/lxl/config/MyAsyncConfig.java
@@ -0,0 +1,31 @@
+package com.lxl.config;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+/**
+ * @Author lixiaolong
+ * @Description: 方式二：自定义配置文件 使用@Async 实现异步线程
+ * @Date 2018/3/28
+ */
+@Configuration
+@EnableAsync
+public class MyAsyncConfig implements AsyncConfigurer {
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(10);
+        taskExecutor.setMaxPoolSize(20);
+        taskExecutor.setQueueCapacity(100);
+        taskExecutor.initialize();
+        return taskExecutor;
+    }
+
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return null;
+    }
+}
